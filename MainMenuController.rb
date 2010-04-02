6b11ad5068b6a94411d10b9e1626705161a0046a
Bradley Buda <brad@tracksimple.com>
2010-04-02 14:44:02 -0700

    Switch to use the growl sample code that comes with MacRuby

--- MainMenuController.rb
+++ MainMenuController.rb
@@ -1,7 +1,13 @@
 class MainMenuController < NSWindowController
+	include Growl
+	GROWL_HELLO = 'Hello'
+
 	attr_accessor :menu
 
 	def awakeFromNib
+		Growl::Notifier.sharedInstance.register('GrowlSample', [GROWL_HELLO])
+		growl GROWL_HELLO, 'OneBusAway Notifier', 'Ready to Serve', :icon => NSImage.imageNamed('onebusaway_icon')
+			
 		bar = NSStatusBar.systemStatusBar
 		@item = bar.statusItemWithLength(NSSquareStatusItemLength)
 		@item.image = NSImage.imageNamed('onebusaway_icon')
